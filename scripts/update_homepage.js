5493a8e987a215a163b9e145739fc1e11f37d038
luis francisco gonzalez jimenez <luisfgv96@hotmail.com>
2022-11-05 21:30:16 -0700

    workflows netlify

---
+++ scripts/update_homepage.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-prototype-builtins */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+import { writeFileSync as save } from "fs";
+
+const pkgJson =
+    require.main.paths[1].split("node_modules")[0]+ "package.json";
+
+const json = require(pkgJson);
+
+if(json.hasOwnProperty("homepage")){
+ json.homepage = "https://rococo-sunflower-9e123e.netlify.app/"
+}
+
+save(pkgJson, JSON.stringify(json, null, 2));
